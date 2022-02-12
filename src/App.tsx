3f77b9515569b388d12ed137ce76a92c47b47201
RomanCox <rom.cox@gmail.com>
2022-02-12 15:09:16 +0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,144 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import style from './App.module.css'
+import styleInput from './common/styles/UpperBlockContainer.module.css'
+import styleLB from './common/styles/LowerBlockContainer.module.css'
+import {DisplayBlock} from './displayBlock/DisplayBlock'
+import {Button} from './button/Button'
+import {StartValueBlock} from "./valueBlock/StartValueBlock";
+import {MaxValueBlock} from "./valueBlock/MaxValueBlock";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+
+    let [startValue, setStartValue] = useState<number>(0)
+    let [maxValue, setMaxValue] = useState<number>(0)
+    let [errorInputStart, setErrorInputStart] = useState<boolean>(false)
+    let [errorInputMax, setErrorInputMax] = useState<boolean>(false)
+    let [error, setError] = useState<number>(0)
+    let [display, setDisplay] = useState<string>("Enter values & press 'SET'")
+    let [buttonSetDisable, setButtonSetDisable] = useState<boolean>(true)
+    let [buttonIncDisable, setButtonIncDisable] = useState<boolean>(true)
+    let [buttonResetDisable, setButtonResetDisable] = useState<boolean>(true)
+
+    useEffect(() => {
+        let newDisplay = sessionStorage.getItem('Display')
+        let errorAsString = sessionStorage.getItem('Error')
+        let startValueAsString = sessionStorage.getItem('Start')
+        let maxValueAsString = sessionStorage.getItem('Max')
+        let buttonSetDisableAsString = sessionStorage.getItem('Set')
+        let buttonIncDisableAsString = sessionStorage.getItem('Inc')
+        let buttonResetDisableAsString = sessionStorage.getItem('Reset')
+        if (
+            newDisplay &&
+            errorAsString &&
+            startValueAsString &&
+            maxValueAsString &&
+            buttonSetDisableAsString &&
+            buttonIncDisableAsString &&
+            buttonResetDisableAsString
+        ) {
+            setDisplay(newDisplay)
+            setError(JSON.parse(errorAsString))
+            setStartValue(JSON.parse(startValueAsString))
+            setMaxValue(JSON.parse(maxValueAsString))
+            setButtonSetDisable(JSON.parse(buttonSetDisableAsString))
+            setButtonIncDisable(JSON.parse(buttonIncDisableAsString))
+            setButtonResetDisable(JSON.parse(buttonResetDisableAsString))
+        }
+    }, [])
+
+    useEffect(() => {
+        sessionStorage.setItem('Display', display)
+        sessionStorage.setItem('Error', JSON.stringify(error))
+        sessionStorage.setItem('Start', JSON.stringify(startValue))
+        sessionStorage.setItem('Max', JSON.stringify(maxValue))
+        sessionStorage.setItem('Set', JSON.stringify(buttonSetDisable))
+        sessionStorage.setItem('Inc', JSON.stringify(buttonIncDisable))
+        sessionStorage.setItem('Reset', JSON.stringify(buttonResetDisable))
+    }, [display, startValue, maxValue])
+
+    const onClickHandlerForSetButton = () => {
+        setDisplay(JSON.stringify(startValue))
+        setError(2)
+        setButtonSetDisable(true)
+        setButtonIncDisable(false)
+    }
+
+    const onClickHandlerForIncButton = () => {
+        setButtonResetDisable(false)
+        let displayNumber = Number(display) + 1
+        setDisplay(String(displayNumber))
+        if (Number(display) + 1 === Number(maxValue)) {
+            setError(3)
+            setButtonIncDisable(true)
+        }
+    }
+
+    const onClickHandlerForResetButton = () => {
+        setDisplay(JSON.stringify(startValue))
+        setError(2)
+        setButtonIncDisable(false)
+        setButtonResetDisable(true)
+    }
+
+    return (
+        <div className={style.appContainer}>
+            <div className={style.blockContainer}>
+                <div className={styleInput.container}>
+                    <MaxValueBlock
+                        error={error}
+                        errorInput={errorInputMax}
+                        startValue={startValue}
+                        maxValue={maxValue}
+                        setMaxValue={setMaxValue}
+                        setErrorInputStart={setErrorInputStart}
+                        setErrorInput={setErrorInputMax}
+                        setDisplay={setDisplay}
+                        setError={setError}
+                        setButtonSetDisable={setButtonSetDisable}
+                        setButtonIncDisable={setButtonIncDisable}
+                        setButtonResetDisable={setButtonResetDisable}
+                    />
+                    <StartValueBlock
+                        error={error}
+                        errorInput={errorInputStart}
+                        startValue={startValue}
+                        maxValue={maxValue}
+                        setStartValue={setStartValue}
+                        setErrorInput={setErrorInputStart}
+                        setErrorInputMax={setErrorInputMax}
+                        setDisplay={setDisplay}
+                        setError={setError}
+                        setButtonSetDisable={setButtonSetDisable}
+                        setButtonIncDisable={setButtonIncDisable}
+                        setButtonResetDisable={setButtonResetDisable}
+                    />
+                </div>
+                <div className={styleLB.lowerBlock}>
+                    <Button
+                        title={'SET'}
+                        onClickHandler={onClickHandlerForSetButton}
+                        disable={buttonSetDisable}
+                    />
+                </div>
+            </div>
+            <div className={style.blockContainer}>
+                <DisplayBlock
+                    display={display}
+                    error={error}
+                />
+                <div className={styleLB.lowerBlock}>
+                    <Button
+                        title={'INC'}
+                        onClickHandler={onClickHandlerForIncButton}
+                        disable={buttonIncDisable}
+                    />
+                    <Button
+                        title={'RESET'}
+                        onClickHandler={onClickHandlerForResetButton}
+                        disable={buttonResetDisable}
+                    />
+                </div>
+            </div>
+        </div>
+    )
 }
-
-export default App;
---
+++ src/button/Button.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './Button.module.css'
+
+type PropsType = {
+    title: 'SET' | 'INC' | 'RESET' | '',
+    onClickHandler: () => void,
+    disable: boolean
+}
+
+export const Button = (props: PropsType) => {
+
+    return (
+        <div className={style.containerButton}>
+            <button className={style.button}
+                    onClick={props.onClickHandler}
+                    disabled={props.disable}
+            >{props.title}</button>
+        </div>
+    )
+}
