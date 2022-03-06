d6294ea495cadd365fb37781f891af9eec6ba27b
RomanCox <rom.cox@gmail.com>
2022-03-06 12:33:44 +0300

    Final commit New Counter with Redux & LocalStorage & Build page

--- src/components/inputOutputBlock/InputBlock.tsx
+++ src/components/inputOutputBlock/InputBlock.tsx
@@ -32,11 +32,20 @@ export const InputBlock = () => {
         dispatch(incButtonAC(true))
         dispatch(resetButtonAC(true))
         if (Number(value) <= 0) {
+            dispatch(setButtonAC(true))
             dispatch(errorInputMaxAC(1))
             dispatch(displayAC('Enter correct values'))
             dispatch(errorAC(1))
-        } else if (Number(value) > 0 && inputsOutput.start.value) {
+        } else if (Number(value) <= Number(inputsOutput.start.value)) {
+            dispatch(setButtonAC(true))
+            dispatch(errorInputMaxAC(1))
+            dispatch(errorInputStartAC(1))
+            dispatch(displayAC('Enter correct values'))
+            dispatch(errorAC(1))
+        }  else if (Number(value) > 0 && Number(value) > Number(inputsOutput.start.value)) {
+            dispatch(setButtonAC(false))
             dispatch(errorInputMaxAC(0))
+            dispatch(errorInputStartAC(0))
             dispatch(displayAC("Enter values & press 'SET'"))
             dispatch(errorAC(0))
         }
@@ -46,14 +55,23 @@ export const InputBlock = () => {
         dispatch(startValueAC(value))
         dispatch(incButtonAC(true))
         dispatch(resetButtonAC(true))
-        if (Number(value) < 0 || Number(inputsOutput.max.value) <= 0) {
+        if (Number(value) < 0) {
+            dispatch(setButtonAC(true))
             dispatch(errorInputStartAC(1))
             dispatch(displayAC('Enter correct values'))
             dispatch(errorAC(1))
-        } else {
+        } else if (Number(value) >= 0 && Number(value) < Number(inputsOutput.max.value)) {
+            dispatch(setButtonAC(false))
             dispatch(errorInputStartAC(0))
+            dispatch(errorInputMaxAC(0))
             dispatch(displayAC("Enter values & press 'SET'"))
             dispatch(errorAC(0))
+        } else if (Number(value) >= Number(inputsOutput.start.value)) {
+            dispatch(setButtonAC(true))
+            dispatch(errorInputMaxAC(1))
+            dispatch(errorInputStartAC(1))
+            dispatch(displayAC('Enter correct values'))
+            dispatch(errorAC(1))
         }
     }
 
